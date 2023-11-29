a84489439e9267afe8b35c164b08c057ab6228cc
Gengsu07 <sugengdatascience@gmail.com>
2023-11-29 16:51:21 +0700

    start footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,19 @@
+import { footerLogo } from "../assets/images";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 max-lg:flex-col ">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="Logo" />
+          </a>
+          <p className="text-white">ddd</p>
+        </div>
+      </div>
+      Footer
+    </footer>
+  );
 };
 
 export default Footer;
